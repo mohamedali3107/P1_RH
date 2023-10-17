1a242cb2935334164e0caaf53f2e1e0f6c91b011
falque <justine.falque@lilo.org>
2023-10-17 17:37:57 +0200

    initiating of database (work in progress), filling architecture

--- src/fill_mysqldb.py
+++ src/fill_mysqldb.py
@@ -10,6 +10,9 @@ import call_to_llm
 import mysql.connector
 import subprocess
 import getpass
+import sql_queries as sql
+import loading.load_pdf as load_pdf
+import loading.load_from_csv as load_csv
 
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.chat_models import ChatOpenAI
@@ -20,12 +23,8 @@ from langchain.prompts import PromptTemplate
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
-template_path = "../data_template_concise.csv"
-cv_path = "../data/"
-complete_paths = [cv_path+file for file in os.listdir(cv_path) if 'pdf' in file]
-
-# Loading the prompt templates (different for each field)
-prompts_df = pd.read_csv("prompts/table_prompts.csv")
+data_dir = "../data/"
+table_prompts_path = "prompts/table_prompts.csv"
 
 
 ########## LLM Chain parameters ##########
@@ -41,34 +40,26 @@ embedding = OpenAIEmbeddings()
 llm_name = 'gpt-3.5-turbo'
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
 
-def fill_one_row(template_path, file, force_refill, save=False, verbose=False):  
+def add_one_cv(db_cursor, doc, table_prompts, force_refill=False, save=True, verbose=False):  
     # todo : plutôt prendre un doc déja loadé par un fichier de loading appelé depuis fill_whole_template
     """
     Inputs:
-        template_path: name of the csv file (it is rather a name than a path...)
-        file: filename of the CV to parse
-        force_refill: if True and if the CV has already been parsed, fills in the same row again anyway
-        save: if True, fills a row in the csv
-        verbose: if True, prints the retrieved chunks and corresponding scores for each field
+        ...
     """
 
-    template_df = pd.read_csv(template_path)
-    fields = list(template_df) 
+    fields = list(table_prompts) 
 
-    if file in template_df["Filename"].unique() and not force_refill:
-        print("This CV has already been parsed")
-        return template_df.loc[template_df["Filename"] == file].values.tolist()[0] # Returns the corresponding row
-    
+    # if already loaded and not force_refill, print and return accordingly
+    if False :
+        return
     else:
         if verbose:
-            print("Filling the template for " + file + "...\n")
-        loader = PyMuPDFLoader(file)
-        cv = loader.load()
-        data = [file] # First field is the filename
+            print("Filling the template for " + doc.metadata['source'] + "...\n")
+        data = [doc.metadata['source']] # First field is the filename
 
         ## Vectorstore and retriever creation
         subprocess.run('rm -rf ' + persist_directory, shell=True)
-        chunks = vectorstore_lib.splitting_of_docs(cv, splitter)
+        chunks = vectorstore_lib.splitting_of_docs(doc, splitter)
         vectordb = vectorstore_lib.new_vectordb(chunks, 
                                 embedding, 
                                 persist_directory
@@ -76,7 +67,6 @@ def fill_one_row(template_path, file, force_refill, save=False, verbose=False):
         retriever_obj = vectordb
 
         for field in fields[1:]:    # Omitting the first field (=filename)
-
             ## Prepare chain with prompt template
             prompt_template = prompts_df[field][0]
             prompt = PromptTemplate(template=prompt_template, input_variables=["context"])
@@ -101,21 +91,43 @@ def fill_one_row(template_path, file, force_refill, save=False, verbose=False):
             print("to implement")
         return data
 
-def fill_whole_template(template_path, complete_paths, print_time, force_refill=True):
+def fill_database(cursor, docs, table_prompts, print_time=True, force_refill=False):
     t0 = time.time()
-    for file in complete_paths:
+    for doc in docs:
         try:
-            fill_one_row(template_path, file, force_refill, save=True, verbose=True)
+            add_one_cv(cursor, doc, table_prompts, force_refill=force_refill, save=True, verbose=True)
         except:
-            print("Error filling the template for " + file)
+            print("Error filling the database for " + doc)
     if print_time:
-        print("Time to fill the template for all CVs: {:.2f}s".format(time.time()-t0))
+        print("Time to fill the database with new CVs : {:.2f}s".format(time.time()-t0))
 
-# file = complete_paths[0]
-# print(fill_one_row(template_path, file, verbose=True))
+def initiate_database(database, verbose=True):
+    cursor = database.cursor()
+    db_name = 'candidates_cv'
+    # check if the database already exists
+    query = f"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{db_name}'"
+    cursor.execute(query)
+    if len(cursor.fetchall()) == 0 :  # a second call fetchall() would return []
+        print("Creating the database...")
+        cursor.execute("CREATE DATABASE " + db_name)
+        database.commit()
+    database.database = db_name
+    cursor.execute(sql.create_candidates)
+    cursor.execute(sql.create_languages)
+    cursor.execute(sql.create_speaks)
+    if verbose :
+        cursor.execute("SHOW TABLES")
+        tables = cursor.fetchall()
+        print("Database tables :")
+        for table in tables:
+            print(table[0])
+            cursor.execute("DESCRIBE " + table[0])
+            columns = cursor.fetchall()
+            for col in columns :
+                print("   ", col)
+
 
 if __name__ == "__main__":
-
     print("Creating/accessing the MySQL database...")
     #username = input("Enter your username : ")
     password = getpass.getpass("Enter your MySQL password: ")
@@ -125,15 +137,9 @@ if __name__ == "__main__":
         password=password,
     )
     cursor = mydb.cursor()
-    db_name = 'candidates_cv'
-    # check if the database already exists
-    query = f"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{db_name}'"
-    cursor.execute(query)
-    if len(cursor.fetchall()) == 0 :  # a second call fetchall() would return []
-        print("Creating the database...")
-        cursor.execute("CREATE DATABASE " + db_name)
-        mydb.commit()
-    mydb.database = db_name
+    initiate_database(mydb)
+    prompts_df = load_csv.load_csv(table_prompts_path)
+    docs = load_pdf.load_files(data_dir, persist_directory=persist_directory, loader_method='PyMuPDFLoader')
 
     cursor.close()
     mydb.close()
---
+++ src/sql_queries.py
@@ -0,0 +1,29 @@
+
+create_candidates = """
+CREATE TABLE IF NOT EXISTS candidates (
+FileName varchar(30) primary key,
+FirstName varchar(30),
+FamilyName varchar(40),
+Email varchar(30),
+PhoneNumber varchar(15),
+LinkedIn varchar(30),
+Webpage varchar(40),
+Country varchar(30)
+);
+"""
+
+create_languages = """
+CREATE TABLE IF NOT EXISTS languages (
+Name varchar(30) primary key
+);
+"""
+
+create_speaks = """
+CREATE TABLE IF NOT EXISTS speaks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    NameLanguage varchar(30),
+    Candidate varchar(30),
+    FOREIGN KEY (NameLanguage) REFERENCES languages (Name),
+    FOREIGN KEY (Candidate) REFERENCES candidates (FileName)
+);
+"""
