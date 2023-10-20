527843d94e4ce67276cbbf394c75799294fc1ed4
falque <justine.falque@lilo.org>
2023-10-20 16:47:50 +0200

    safety check to avoid non pdf files

--- src/loading/utils.py
+++ src/loading/utils.py
@@ -1,7 +1,7 @@
 import os
 
 def list_of_files(data_dir) :
-    return os.listdir(data_dir)
+    return [file for file in os.listdir(data_dir) if '.pdf' in file]
 
 def list_of_files_as_str(data_dir) :
     joint = ', ' + data_dir
